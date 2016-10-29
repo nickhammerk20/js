615236a02c6ad93921c7f3cefc5c84b0370aa6aa
nickhammerk20 <kolich.x10@gmail.com>
2016-10-29 17:28:50 +0300

    lesson 2016.10.29

--- Lesson2016.10.29/replace.js
+++ Lesson2016.10.29/replace.js
@@ -1,3 +1,37 @@
+
 /**
  * Created by hammer on 29.10.2016.
  */
+
+function replace() {
+    var xxx = document.getElementById("mainVideo");
+    console.log(xxx);
+    var yyy = xxx.getElementsByTagName("iFrame");
+    console.log(yyy);
+    yyy[0].setAttribute("src", "https://www.youtube.com/embed/DyMcnGgRfq8?autoplay=1");
+}
+replace();
+
+function replace() {
+    var xxx = document.getElementById("mainVideo");
+    console.log(xxx);
+    var yyy = xxx.getElementsByTagName("iFrame");
+    console.log(yyy);
+    yyy[0].src = "https://www.youtube.com/embed/DyMcnGgRfq8?autoplay=1";
+}
+replace();
+
+
+function replace() {
+    var frame = document.getElementsByTagName("iFrame");
+    frame[0].src = " https://www.youtube.com/embed/DyMcnGgRfq8?autoplay=1";
+}
+replace();
+
+function replace() {
+
+    var con = document.getElementById("mainVideo");
+    var iframe = con.childNodes[1];
+    iframe.src = "https://www.youtube.com/embed/yqQwH_Qj1x0?autoplay=1";
+}
+replace();
--- lesson20161022/cars_next.js
+++ lesson20161022/cars_next.js
@@ -10,7 +10,7 @@ var carToyota = {
 }
 var carFord = {
     name: "mustang",
-    year: 1962,
+    year: "1962",
     engine: 6200,
     owner: {
         name: "vasya",
@@ -47,3 +47,12 @@ function printProperty(car) {
 }
 printProperty(carFord);
 
+var xxx = JSON.stringify(carToyota);
+console.log(xxx);
+
+var carFordMass = '{"name": "mustang","year": "1962","engine": "6200"}';
+console.log(carFordMass);
+
+var yyy = JSON.parse(carFordMass);
+console.log(yyy);
+printProperty(yyy);
